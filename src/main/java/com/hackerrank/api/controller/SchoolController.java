5d4652939424fc1ceb165146ae944ecd50344e2a
Hien Ng <hien.nguyenhuuquoc@axonactive.com>
2024-05-20 23:32:42 +0700

    Your commit message

--- src/main/java/com/hackerrank/api/controller/SchoolController.java
+++ src/main/java/com/hackerrank/api/controller/SchoolController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @RestController
@@ -27,6 +29,7 @@ public class SchoolController {
     @PostMapping("/teacher")
     public ResponseEntity<Teacher> createTeacher(@RequestBody Teacher teacher) {
         Teacher _teacher = teacherRepository.save(teacher);
+
         return new ResponseEntity<>(_teacher, HttpStatus.CREATED);
     }
 
@@ -40,21 +43,51 @@ public class SchoolController {
     //add student to a teacher
     @PostMapping("/teacher/{teacherId}/addStudent")
     public ResponseEntity<Teacher> addStudent(@PathVariable(value = "teacherId") Long teacherId, @RequestBody Student addStudent) {
-        //TODO
-        return null;
+        Optional<Teacher> teacherOptional = teacherRepository.findById(teacherId);
+        if (!teacherOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Teacher teacher = teacherOptional.get();
+        Optional<Student> studentOptional = studentRepository.findById(addStudent.getId());
+        Student student;
+        if (studentOptional.isPresent())
+        {
+            student = studentOptional.get();
+        }
+        else
+        {
+            student = studentRepository.save(addStudent);
+        }
+
+        teacher.getStudents().add(student);
+        student.getTeachers().add(teacher);
+
+        teacher = teacherRepository.save(teacher);
+        studentRepository.save(student);
+        return new ResponseEntity<>(teacher, HttpStatus.OK);
     }
 
     //get students of a teacher
     @GetMapping("/teacher/{teacherId}/students")
     public ResponseEntity<Set<Student>> getStudentsOfATeacher(@PathVariable(value = "teacherId") Long teacherId) {
-        //TODO
-        return null;
+        Optional<Teacher> teacherOptional = teacherRepository.findById(teacherId);
+        if (!teacherOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Set<Student> students = teacherOptional.get().getStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 
     // get teachers of a student
     @GetMapping("/student/{studentId}/teachers")
     public ResponseEntity<Set<Teacher>> getTeachersOfAStudent(@PathVariable(value = "studentId") Long studentId) {
-        //TODO
-        return null;
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        if (!studentOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Set<Teacher> teachers = studentOptional.get().getTeachers();
+        return new ResponseEntity<>(teachers, HttpStatus.OK);
     }
 }
